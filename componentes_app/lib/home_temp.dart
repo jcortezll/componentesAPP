1a445c75582ba32062809eb641675277f7ed94a3
Joan Cortez Llorca <joluenti@gmail.com>
2023-05-30 17:59:41 +0200

    Página 25

---
+++ componentes_app/lib/home_temp.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  HomePageTemp({super.key});
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes Temp'),
+        ),
+        body: ListView(children: _crearItemsCorta()));
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> listaW = [];
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      listaW
+        ..add(tempWidget)
+        ..add(const Divider());
+    }
+
+    return listaW;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [ListTile(
+          title: Text('$item!'),
+          subtitle: const Text('Probando atributos'),
+          leading: const Icon(Icons.accessible_sharp),
+          trailing: const Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            
+          },
+          ), 
+          const Divider()],
+      );
+    }).toList();
+  }
+}
---
+++ componentes_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:componentes_app/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp()
+    );
+  }
+}
